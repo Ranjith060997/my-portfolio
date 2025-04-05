5125c84e7701b95eab9b7a507bb7f6caa79a02b8
Renjith <renjith@Renjiths-MacBook-Air.local>
2025-04-05 13:48:53 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,246 @@
+import project1 from "../images/sf.jpg";
+import project2 from "../images/sfa-airtel.png";
+
+export const Bio = {
+  name: "Ranjith Kumar",
+  roles: [
+    "Mobile Apps Engineer",
+    "React Native Developer",
+    "Software Engineer",
+  ],
+  description:
+    "I am a passionate and adaptable developer, always excited to tackle new challenges. Committed to continuous learning, I strive to deliver impactful solutions. With a problem-solving mindset and a drive for excellence, I’m eager to contribute, grow, and make a difference in every project I take on",
+  github: "https://github.com/Ranjith060997",
+  resume:
+    "https://drive.google.com/file/d/1l9OvQ4GFDJjAhuYy1mKEdScIxh_hEQ7t/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ranjith-kumar-3499001a0/",
+  mailId: "ranjithkumar691997@gmail.com",
+  mailLink:
+    "https://mail.google.com/mail/?view=cm&fs=1&to=ranjithkumar691997@gmail.com&su=New opportunity&body=Hi RanjithKumar K",
+};
+
+export const skills = [
+  {
+    title: "Cross Platform",
+    skills: [
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "React Native",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux Toolkit",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Expo",
+        image:
+          "https://play-lh.googleusercontent.com/algsmuhitlyCU_Yy3IU7-7KYIhCBwx5UJG4Bln-hygBjjlUVCiGo1y8W5JNqYm9WW3s",
+      },
+    ],
+  },
+
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image: "https://www.svgrepo.com/show/184143/java.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Gradle",
+        image:
+          "https://gradle.org/images/gradle-knowledge-graph-logo.png?20170228",
+      },
+      {
+        name: "Google Play Console",
+        image:
+          "https://play-lh.googleusercontent.com/4_RW0mQ5mJhGGJVydTlQsQ0pnqAYq9UoTVm2_gElrgRM13Q02w43HPgYVaMFy4b4smmF=w240-h480-rw",
+      },
+      {
+        name: "Firebase Cloud Messaging",
+        image:
+          "https://static.invertase.io/assets/firebase/cloud-messaging.svg",
+      },
+    ],
+  },
+  {
+    title: "iOS",
+    skills: [
+      {
+        name: "XCode",
+        image:
+          "https://developer.apple.com/assets/elements/icons/xcode-12/xcode-12-96x96_2x.png",
+      },
+      {
+        name: "App Store Connect",
+        image:
+          "https://developer.apple.com/assets/elements/icons/app-store-connect/app-store-connect-256x256_2x.png",
+      },
+      {
+        name: "IOS Development",
+        image:
+          "https://www.peanutsquare.com/wp-content/uploads/2024/04/ios-1.png",
+      },
+    ],
+  },
+  {
+    title: "CI/CD & Devops",
+    skills: [
+      {
+        name: "FastLane",
+        image: "https://www.svgrepo.com/show/376368/fastlane.svg",
+      },
+      {
+        name: "GitHub Actions",
+        image:
+          "https://www.opsmx.com/wp-content/uploads/2021/08/GitHub-Actions.svg",
+      },
+    ],
+  },
+  {
+    title: "Security",
+    skills: [
+      {
+        name: "JWT",
+        image: "https://cdn.worldvectorlogo.com/logos/jwt-3.svg",
+      },
+      {
+        name: "OAuth",
+        image: "https://www.svgrepo.com/show/361916/oauth.svg",
+      },
+      {
+        name: "MicroSoft Authenticator",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/microsoft-authenticator.svg",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+      {
+        name: "Tortoise Git",
+        image: "https://icon.icepanel.io/Technology/svg/TortoiseGit.svg",
+      },
+      {
+        name: "Putty",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/PuTTY_Icon.svg/800px-PuTTY_Icon.svg.png?20220831174608",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://enhancesys-files.s3.ap-south-1.amazonaws.com/uploads/public/669/0f7/04e/6690f704ef3e8335420923.png",
+    role: "Software Engineer",
+    company: "Enhancesys Innovations Pvt Ltd",
+    date: "May 2022 - Present",
+    desc: `
+Over the past four years, I have been deeply involved in analyzing user requirements, designing intuitive interfaces, and developing robust features using JavaScript, Java, and related technologies. My focus has been on building scalable and maintainable applications that ensure long-term software stability. Currently, I lead a team of 9 developers, driving the development of cross-platform mobile applications with React Native. Managing 7 projects simultaneously—5 support and 2 ongoing—I ensure smooth project execution while delivering high-quality solutions.
+    `,
+    skills: ["JS", "React", "React Native", "Android and iOS Development"],
+    doc: "https://enhancesys.com/storage/temp/public/986/5c3/99d/enhancesyslogo__834.png",
+  },
+  {
+    id: 1,
+    img: "https://enhancesys-files.s3.ap-south-1.amazonaws.com/uploads/public/669/0f7/04e/6690f704ef3e8335420923.png",
+    role: "Graduate Engineer Trainee",
+    company: "Enhancesys Innovations Pvt Ltd",
+    date: "May 2021 - May 2022",
+    desc: `
+As a Graduate Engineer Trainee at Enhancesys Innovations, I played a key role in code reviews, collaborating with QA teams, and troubleshooting issues to enhance user experience. I also handled app submissions and updates for iOS and Android, ensuring compliance with store guidelines. Working closely with product managers and stakeholders, I helped prioritize tasks and streamline project delivery. Additionally, I mentored junior developers and contributed to continuous process improvements, reinforcing a culture of learning and efficiency.
+    `,
+    skills: [
+      "JS",
+      "React",
+      "React Native",
+      "Android and iOS Development",
+      "jQuery",
+      "Apache Cordova",
+    ],
+    doc: "https://enhancesys.com/storage/temp/public/986/5c3/99d/enhancesyslogo__834.png",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQF2n--iDbZBTQ/company-logo_200_200/company-logo_200_200/0/1630627756732/institute_of_road_and_transport_technology_logo?e=1749081600&v=beta&t=DXuI_XtKeX0kvPuconjcHwC4m3FtFsoGg4Aiul9W7vc",
+    school: "Institute of Road and Transport Technology",
+    date: "Aug 2015 - June 2019",
+    grade: "6.25 CGPA",
+    desc: "I transitioned from hardware engineering to software engineering, leveraging my technical expertise to develop efficient, scalable, and innovative software solutions.",
+    degree: "Bachelor of Engineering - Electricals & Electronics",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Viva Apps",
+    date: "Jan 2024 - Present",
+    description: `This application helps users efficiently manage day-to-day business operations.`,
+    image: project1,
+    tags: ["React Js", "React Native", "Redux Toolkit"],
+    category: "react native",
+    github:
+      "https://play.google.com/store/apps/details?id=com.smartfren.snoc&hl=en_IN",
+  },
+  {
+    id: 1,
+    title: "SFA - Airtel",
+    date: "Jan 2022 - Present",
+    description:
+      "The SFA Airtel app streamlines sales and field operations, enabling customer management, sales tracking, order processing, and performance analytics for Airtel representatives.",
+    image: project2,
+    tags: ["React Js", "React Native", "Redux"],
+    category: "react native",
+    github: "https://apps.apple.com/in/app/sfa-airtel/id6477820855",
+    member: [],
+  },
+];
